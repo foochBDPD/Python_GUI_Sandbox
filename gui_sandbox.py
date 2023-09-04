04a0afd9b0b1f010e4c71f0391b233bd12abd317
fooch <m.p.palladino@gmail.com>
2023-09-04 11:26:52 -0400

    Clicking a button now

--- gui_sandbox.py
+++ gui_sandbox.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 import tkinter as tk
+import subprocess
+
+def start_tetris():
+    subprocess.run(["python", "tetris_game.py"])  # Replace with the actual Tetris game script
 
 def main():
     window = tk.Tk()
-    label = tk.Label(
-    text="Hello, Tkinter",
-    fg="white",
-    bg="black",
-    width=50,
-    height=50
-)
-    label.pack()
+    window.title("GUI with Tetris Launcher")
+
+    label = tk.Label(window, text="Click the button to start Tetris:")
+    label.pack(pady=10)
+
+    button = tk.Button(window, text="Click Me", command=start_tetris)
+    button.pack()
+
     window.mainloop()
 
 if __name__ == "__main__":
